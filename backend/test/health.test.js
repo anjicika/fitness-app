04fc555ac2e78009f3d4ece3ae32de95c730738e
gasper <gasper.potocnik2@student.um.si>
2025-12-18 01:18:23 +0100

    13

--- backend/test/health.test.js
+++ backend/test/health.test.js
@@ -1,6 +1,6 @@
 // backend/__tests__/health.test.js
-describe("health check", () => {
-  it("should pass", () => {
+describe('health check', () => {
+  it('should pass', () => {
     expect(true).toBe(true);
   });
 });

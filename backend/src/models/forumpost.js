ffb6ce3afb722a0498b263faf8e38eaabcf920d5
Lazar Ilic <iliclazar16@gmail.com>
2025-12-23 14:33:14 +0100

    feat: database models and migrations for user, workout and forum-post

---
+++ backend/src/models/forumpost.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ForumPost extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ForumPost.init({
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    userId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ForumPost',
+  });
+  return ForumPost;
+};

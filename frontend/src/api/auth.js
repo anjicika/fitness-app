b8ea559f7bf88727422c53d9c214dca988816a3b
Elv4L0w <91699930+Elv4L0w@users.noreply.github.com>
2026-01-11 21:59:15 +0100

    Feat: I had a problem with register function so I reimplemented it here if you can find some other way to go around it when we merge do it

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,5 +1,14 @@
 const API_URL = 'http://localhost:3000/api/v1/auth';
 
+export async function register({ username, email, password }) {
+  const res = await fetch(`${API_URL}/register`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, email, password }),
+  });
+  return res.json();
+}
+
 export async function login({ email, password }) {
   const res = await fetch(`${API_URL}/login`, {
     method: 'POST',
